fb75113bc9113385b0d949a02e128182c8f02493
Mehrdad Karami <studiometao@gmail.com>
2016-04-08 17:31:27 +0430

    angular robot

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, './client')));
 
 
 var server = require('http').Server(app);

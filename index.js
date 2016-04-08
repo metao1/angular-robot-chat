f1a89dfa17c0f60b250893baf9cb16126a7d2a95
Mehrdad Karami <studiometao@gmail.com>
2016-04-08 17:44:41 +0430

    angular robot

--- index.js
+++ index.js
@@ -13,7 +13,11 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'client')));
-
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 var server = require('http').Server(app);
 app.set('port', (process.env.PORT || 5000));

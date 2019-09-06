e3cdf29eb325f4a34aff10775977a1a66a670c42
Mr.Code <mehrdadallahkarami@gmail.com>
2019-09-06 16:52:18 +0200

    modify port

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var app = express();
 
 app.use(express.static(path.join(__dirname, 'client')));
 
-app.listen(8000, '0.0.0.0', function() {});
+app.listen(process.env.PORT || 5000)
 
 module.exports = app;
 

8f8d35983c3c8c23ba42ab7bddb78f9e527a4b2d
Mr.Code <mehrdadallahkarami@gmail.com>
2019-09-06 17:18:09 +0200

    add controller

--- app.js
+++ app.js
@@ -6,8 +6,13 @@ var request = require('request');
 
 var app = express();
 
-app.use(express.static(path.join(__dirname, '../client')));
-
+app.use(express.static(path.join(__dirname, 'client')));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 var port = process.env.PORT || 80;
 app.listen(port);
 
